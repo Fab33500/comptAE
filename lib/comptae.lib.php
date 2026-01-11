bc061abb58ec1c98f3c7d5d6b50ca1804602c543
Fab33500 <37309745+Fab33500@users.noreply.github.com>
2026-01-11 10:34:19 +0100

    1ere instalation du module

---
+++ lib/comptae.lib.php
@@ -0,0 +1,85 @@
+<?php
+/* Copyright (C) 2026		SuperAdmin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    comptae/lib/comptae.lib.php
+ * \ingroup comptae
+ * \brief   Library files with common functions for ComptAE
+ */
+
+/**
+ * Prepare admin pages header
+ *
+ * @return array<array{string,string,string}>
+ */
+function comptaeAdminPrepareHead()
+{
+	global $langs, $conf;
+
+	// global $db;
+	// $extrafields = new ExtraFields($db);
+	// $extrafields->fetch_name_optionals_label('myobject');
+
+	$langs->load("comptae@comptae");
+
+	$h = 0;
+	$head = array();
+
+	$head[$h][0] = dol_buildpath("/comptae/admin/setup.php", 1);
+	$head[$h][1] = $langs->trans("Settings");
+	$head[$h][2] = 'settings';
+	$h++;
+
+	/*
+	$head[$h][0] = dol_buildpath("/comptae/admin/myobject_extrafields.php", 1);
+	$head[$h][1] = $langs->trans("ExtraFields");
+	$nbExtrafields = (isset($extrafields->attributes['myobject']['label']) && is_countable($extrafields->attributes['myobject']['label'])) ? count($extrafields->attributes['myobject']['label']) : 0;
+	if ($nbExtrafields > 0) {
+		$head[$h][1] .= '<span class="badge marginleftonlyshort">' . $nbExtrafields . '</span>';
+	}
+	$head[$h][2] = 'myobject_extrafields';
+	$h++;
+
+	$head[$h][0] = dol_buildpath("/comptae/admin/myobjectline_extrafields.php", 1);
+	$head[$h][1] = $langs->trans("ExtraFieldsLines");
+	$nbExtrafields = (isset($extrafields->attributes['myobjectline']['label']) && is_countable($extrafields->attributes['myobjectline']['label'])) ? count($extrafields->attributes['myobject']['label']) : 0;
+	if ($nbExtrafields > 0) {
+		$head[$h][1] .= '<span class="badge marginleftonlyshort">' . $nbExtrafields . '</span>';
+	}
+	$head[$h][2] = 'myobject_extrafieldsline';
+	$h++;
+	*/
+
+	$head[$h][0] = dol_buildpath("/comptae/admin/about.php", 1);
+	$head[$h][1] = $langs->trans("About");
+	$head[$h][2] = 'about';
+	$h++;
+
+	// Show more tabs from modules
+	// Entries must be declared in modules descriptor with line
+	//$this->tabs = array(
+	//	'entity:+tabname:Title:@comptae:/comptae/mypage.php?id=__ID__'
+	//); // to add new tab
+	//$this->tabs = array(
+	//	'entity:-tabname:Title:@comptae:/comptae/mypage.php?id=__ID__'
+	//); // to remove a tab
+	complete_head_from_modules($conf, $langs, null, $head, $h, 'comptae@comptae');
+
+	complete_head_from_modules($conf, $langs, null, $head, $h, 'comptae@comptae', 'remove');
+
+	return $head;
+}
